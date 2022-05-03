6ec488ba54e8f14ed5f9fedc8b14049c183dca7a
Georgii Panasenko <103067076+Qilok@users.noreply.github.com>
2022-05-03 19:13:53 +0000

    STD19

--- student.py
+++ student.py
@@ -71,7 +71,7 @@ class Piggy(PiggyParent):
           
     def dance(self):
       self.safe_to_dance()
-      if self.read_distance() >= 500:
+      if self.read_distance() >= 600:
         response = str.lower(input("Move left or move right(l/r): "))
         if response == 'l':
           self.left(primary=90, counter=-90)
