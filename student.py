5dbfe258870d68d4d1b75b1179a2f0660ac7b210
Georgii Panasenko <103067076+Qilok@users.noreply.github.com>
2022-05-03 18:39:08 +0000

    STD3

--- student.py
+++ student.py
@@ -60,36 +60,42 @@ class Piggy(PiggyParent):
 
     def safe_to_dance(self):
         """ Does a 360 distance check and returns true if safe """
-        for safty in range (2):
+        for safty in range (4):
           self.servo(1000)
           self.read_distance()
           self.servo(2000)
           self.read_distance()
-          self.right(primary=90, counter=-90)
-          time.sleep(1)
+          self.right()
+          time.sleep(0.5)
 
           
     def dance(self):
-      response = str.lower(input("Move left or move right(l/r): "))
-      if response == 'l':
-        self.left(primary=90, counter=-90)
-      elif response == 'r':
-        self.right(primary=90, counter=-90)
-      time.sleep(5)
-      self.fwd()
-      time.sleep(2) 
-      for butty in range(4):
-        self.right()
-        time.sleep(0.4)
-        self.left()
-        time.sleep(0.4)
+      self.safe_to_dance
+      if self.read_distance <= 600
         
-      for butty2 in range(4):
-        self.right(primary=90, counter=-90)
-        time.sleep(0.2)
-        self.left(primary=90, counter=-90)
-        time.sleep(0.2)
-      self.stop()
+        response = str.lower(input("Move left or move right(l/r): "))
+        if response == 'l':
+          self.left(primary=90, counter=-90)
+        elif response == 'r':
+          self.right(primary=90, counter=-90)
+        time.sleep(5)
+        self.fwd()
+        time.sleep(2) 
+        for butty in range(4):
+          self.right()
+          time.sleep(0.4)
+          self.left()
+          time.sleep(0.4)
+          
+        for butty2 in range(4):
+          self.right(primary=90, counter=-90)
+          time.sleep(0.2)
+          self.left(primary=90, counter=-90)
+          time.sleep(0.2)
+        self.stop()
+      
+      else
+        self.stop()
 
     def square(self):
         for edge in range(4):
